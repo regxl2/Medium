7f4c0b75502e8c4fdb2799f4d9124e881f8a4de2
Abhishek Rathore <abhishekrathore@regxl-2.local>
2024-08-23 03:36:23 +0530

    added images of android app

--- app/src/main/java/com/example/medium/di/AppModule.kt
+++ app/src/main/java/com/example/medium/di/AppModule.kt
@@ -1,15 +1,10 @@
 package com.example.medium.di
 
-import android.content.Context
 import com.example.medium.data.remote.MediumApi
-import com.example.medium.data.repository.TokenRepositoryImpl
-import com.example.medium.domain.repository.TokenRepository
-import com.example.medium.domain.usecases.GetTokenUseCase
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
